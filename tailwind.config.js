aed480306446db75ca368f04123a8bcbab0a6739
mister025e <mister025.2.0@gmail.com>
2024-10-31 23:57:47 +0100

    minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     './app//*.{php,html}',    // Include all the PHP et HTML files in app folder
-    './app/styles.css',         // Include main CSS file if using classes Tailwind inside
+    './app/styles/styles.css',         // Include main CSS file if using classes Tailwind inside
   ],
   theme: {
     extend: {},
